68346194df273b369ab224b22fb7ec6de1db6c63
Mac Ty <esenetheodoremacaulay3@gmail.com>
2026-06-21 23:26:29 +0100

    GCTU website version 1.0

---
+++ script.js
@@ -0,0 +1,34 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+    (window.scrollY > 140) ? header.classList.add("scrolled") : header.classList.remove("scrolled");
+})
+
+const hamburgerButton = document.querySelector(".hamburger-bttn");
+const primaryNav = document.querySelector(".primary-navigation");
+
+hamburgerButton.addEventListener("click", () => {
+    const isOpened = hamburgerButton.getAttribute("aria-expanded");
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (isOpened === "false" && visibility === "false") {
+        hamburgerButton.setAttribute("aria-expanded", "true");
+        primaryNav.setAttribute("data-visible", "true");
+        header.classList.add("scrolled");
+    }
+    else {
+        hamburgerButton.setAttribute("aria-expanded", "false");
+        primaryNav.setAttribute("data-visible", "false");
+    }
+}); 
+
+
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        document.querySelectorAll(".tab").forEach(t => {
+            t.classList.remove("active");
+        })
+        tab.classList.add("active");
+    });
+});
